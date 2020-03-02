65b7df6944c18bf11586776daed849c9865e01bc
Jorge Diaz <jorgdiaz.lara@gmail.com>
2020-03-02 16:47:59 -0600

    Solución al reto

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 # Resolve the problem!!
+import re
+
 
 
 def run():
-    # Start coding here
+    with open ('encoded.txt', 'r', encoding='utf-8') as f:
+        texto_cifrado = f.read()
+    mensaje = ""
+    mensaje = re.findall("[a-z]", texto_cifrado)
+    mensaje_oculto = ''.join(mensaje)
+    print(mensaje_oculto)
 
 
 if __name__ == '__main__':
